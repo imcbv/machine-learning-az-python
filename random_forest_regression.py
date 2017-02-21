2c5c97b27cd4a8b3298f96f5be37f42c5f1dc25c
Christian Vismara <imcbv@MacBook-Pro-di-Christian.local>
2017-02-21 19:48:02 +0100

    classification until naive bayes

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 15 08:37:30 2017
+Created on Thu Feb 16 00:29:09 2017
 
 @author: imcbv
 """
 
-# Regression Template
+# Random Forest Regression
 
 # Importing the libraries
 import numpy as np
@@ -18,29 +18,22 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
                 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X = sc_X.fit_transform(X) 
-sc_y = StandardScaler()
-y = sc_y.fit_transform(y) 
-                
-# Fitting SVR to the dataset
-from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
+# Fitting the Regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
 regressor.fit(X, y)
 
-# Predicting a new result with inverse scaling
-sc_x = sc_X.transform(np.array([[6.5]]))
-y_pred = sc_y.inverse_transform(regressor.predict(sc_x))
+# Predicting a new result
+x = 6.5
+y_pred = regressor.predict(x)
 
 
 # Visualising the Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (SVR Model)')
+plt.title('Truth or Bluff (Decision tree regressor)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
